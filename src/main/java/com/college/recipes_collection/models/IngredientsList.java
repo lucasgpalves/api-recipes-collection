20825f93a45760cf18e3381404134d1f139741f4
Lucas Alves <alvez.lucas2702@gmail.com>
2024-08-29 22:43:43 -0300

    feat: updated the ingredient list class adding  the attributes

--- src/main/java/com/college/recipes_collection/models/IngredientsList.java
+++ src/main/java/com/college/recipes_collection/models/IngredientsList.java
@@ -1,13 +1,16 @@
 package com.college.recipes_collection.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "ingredients_list")
 public class IngredientsList {
     
     @Id
@@ -17,4 +20,15 @@ public class IngredientsList {
     @ManyToOne
     @JoinColumn(name = "recipe_id", referencedColumnName = "id")
     private Recipe recipe;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id", referencedColumnName = "id")
+    private Ingredient ingredient;
+
+    @ManyToOne
+    @JoinColumn(name = "measurement_id", referencedColumnName = "id")
+    private Measurement measurement;
 }
