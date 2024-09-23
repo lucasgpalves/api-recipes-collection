db4571a6855e10ad9456a9fc8b42fc111fe405e1
Lucas Alves <alvez.lucas2702@gmail.com>
2024-09-23 14:35:34 -0300

    fix: error in revenue controller put to delete

--- src/main/java/com/college/recipes_collection/controllers/RevenueController.java
+++ src/main/java/com/college/recipes_collection/controllers/RevenueController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class RevenueController {
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteRevenueById(@PathVariable Long id) {
         revenueService.deleteRevenueById(id);
         return ResponseEntity.noContent().build();
