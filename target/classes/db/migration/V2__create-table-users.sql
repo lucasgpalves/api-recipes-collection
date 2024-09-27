98c52f22185388f826f30026b23679d1949fee43
Lucas Alves <alvez.lucas2702@gmail.com>
2024-09-27 12:50:04 -0300

    fix: update migration users

--- target/classes/db/migration/V2__create-table-users.sql
+++ target/classes/db/migration/V2__create-table-users.sql
@@ -4,7 +4,8 @@ CREATE TABLE users(
     name VARCHAR(80) NOT NULL,
     role_id INT NOT NULL,
     salary DECIMAL(9,2) NOT NULL,
-    ingressed_at TIMESTAMP,
+    ingressed_at TIMESTAMP NOT NULL,
+    terminated_at TIMESTAMP,
     fantasy_name VARCHAR(80) NULL,
 
     FOREIGN KEY (role_id) REFERENCES roles(id)
