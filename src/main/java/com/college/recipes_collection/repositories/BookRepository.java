b17431998d17f4d0daa83e9f67d5ee71e959e0b0
Lucas Alves <alvez.lucas2702@gmail.com>
2024-09-29 18:47:23 -0300

    fix: add annotations

--- src/main/java/com/college/recipes_collection/repositories/BookRepository.java
+++ src/main/java/com/college/recipes_collection/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package com.college.recipes_collection.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.college.recipes_collection.models.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
     
 }
